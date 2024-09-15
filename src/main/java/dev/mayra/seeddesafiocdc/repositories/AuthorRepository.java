8156f9249d866aa56e3ccea28970496fc333e88b
Mayra Amaral <mayracunha18@gmail.com>
2024-09-14 23:11:02 -0300

    feat: adiciona repository do autor

---
+++ src/main/java/dev/mayra/seeddesafiocdc/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package dev.mayra.seeddesafiocdc.repositories;
+
+import dev.mayra.seeddesafiocdc.model.author.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    boolean existsByEmail(String email);
+}
