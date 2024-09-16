3b3dd17cdb85be56feb485fa33d5a9c228715f40
Mayra Amaral <mayracunha18@gmail.com>
2024-09-15 23:43:48 -0300

    feat: adiciona schemas nas controllers

--- src/main/java/dev/mayra/seeddesafiocdc/model/author/AuthorRequestDTO.java
+++ src/main/java/dev/mayra/seeddesafiocdc/model/author/AuthorRequestDTO.java
@@ -1,6 +1,7 @@
 package dev.mayra.seeddesafiocdc.model.author;
 
 import dev.mayra.seeddesafiocdc.utils.validators.UniqueValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,15 +9,20 @@ import jakarta.validation.constraints.Size;
 public class AuthorRequestDTO {
 
     @NotNull(message = "Name can't be null")
+    @Schema(description = "Fill the author name", required = true, example = "Érico Veríssimo")
     private String name;
 
     @NotNull(message = "E-mail can't be null")
     @Email(message = "E-mail must be valid")
     @UniqueValue(fieldName = "email", domainClass = Author.class, message = "E-mail already in use")
+    @Schema(description = "Fill the author e-mail", required = true, example = "example@email.com")
     private String email;
 
     @NotNull(message = "Description can't be null")
     @Size(max = 400, message = "Description can have a maximum of 400 characters")
+    @Schema(description = "Fill the author description", required = true, example = "Erico Lopes Verissimo foi um " +
+        "escritor brasileiro. Com uma prosa simples e de fácil leitura, tornou-se um dos escritores mais " +
+        "populares da literatura brasileira.")
     private String description;
 
     public AuthorRequestDTO(String name, String email, String description) {
--- src/main/java/dev/mayra/seeddesafiocdc/model/category/CategoryRequestDTO.java
+++ src/main/java/dev/mayra/seeddesafiocdc/model/category/CategoryRequestDTO.java
@@ -1,12 +1,14 @@
 package dev.mayra.seeddesafiocdc.model.category;
 
 import dev.mayra.seeddesafiocdc.utils.validators.UniqueValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public class CategoryRequestDTO {
 
     @NotNull(message = "Name can't be null")
     @UniqueValue(fieldName = "name", domainClass = Category.class, message = "Name already exists")
+    @Schema(description = "Fill the category name", required = true, example = "Romance")
     private String name;
 
     @Deprecated
