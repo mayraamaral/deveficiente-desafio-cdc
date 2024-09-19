335ca5062dad58a9a96f4d06355efcdaae750cd4
Mayra Amaral <mayracunha18@gmail.com>
2024-09-19 20:16:15 -0300

    refactor: adiciona lógica de salvar compra no item

--- src/main/java/dev/mayra/seeddesafiocdc/model/purchaseItem/PurchaseItem.java
+++ src/main/java/dev/mayra/seeddesafiocdc/model/purchaseItem/PurchaseItem.java
@@ -1,20 +1,23 @@
 package dev.mayra.seeddesafiocdc.model.purchaseItem;
 
 import dev.mayra.seeddesafiocdc.model.book.Book;
+import dev.mayra.seeddesafiocdc.model.purchase.Purchase;
 
 public class PurchaseItem {
     private Long id;
     private Book book;
     private Long quantity;
+    private Purchase purchase;
 
     public PurchaseItem(Book book, Long quantity) {
         this.book = book;
         this.quantity = quantity;
     }
 
-    public PurchaseItem(PurchaseItemRequestDTO dto, Book book) {
+    public PurchaseItem(PurchaseItemRequestDTO dto, Book book, Purchase purchase) {
         this.book = book;
         this.quantity = dto.getQuantity();
+        this.purchase = purchase;
     }
 
     public Long getId() {
@@ -27,6 +30,14 @@ public class PurchaseItem {
 
     public Long getQuantity() {
         return quantity;
+    }
+
+    public Purchase getPurchase() {
+        return purchase;
+    }
+
+    public void setPurchase(Purchase purchase) {
+        this.purchase = purchase;
     }
 
     public PurchaseItemResponseDTO toResponseDTO() {
