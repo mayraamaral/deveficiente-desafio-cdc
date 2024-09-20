cf76e25cd00e92dc95439633185e5ba22af7dde3
Mayra Amaral <mayracunha18@gmail.com>
2024-09-20 18:44:07 -0300

    feat: adiciona named query de findByCode no CouponRepository

--- src/main/java/dev/mayra/seeddesafiocdc/repositories/CouponRepository.java
+++ src/main/java/dev/mayra/seeddesafiocdc/repositories/CouponRepository.java
@@ -3,5 +3,8 @@ package dev.mayra.seeddesafiocdc.repositories;
 import dev.mayra.seeddesafiocdc.model.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    Optional<Coupon> findByCode(String code);
 }
