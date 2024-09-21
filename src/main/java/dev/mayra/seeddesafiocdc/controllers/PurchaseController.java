9ccdd7fb1ea1f742eb27143a2f794ac42ffd7f1a
Mayra Amaral <mayracunha18@gmail.com>
2024-09-21 15:26:40 -0300

    refactor: cria PurchaseService para conter a lógica da compra

--- src/main/java/dev/mayra/seeddesafiocdc/controllers/PurchaseController.java
+++ src/main/java/dev/mayra/seeddesafiocdc/controllers/PurchaseController.java
@@ -9,6 +9,7 @@ import dev.mayra.seeddesafiocdc.model.purchaseItem.PurchaseItemRequestDTO;
 import dev.mayra.seeddesafiocdc.model.state.State;
 import dev.mayra.seeddesafiocdc.model.validation.ValidationResult;
 import dev.mayra.seeddesafiocdc.repositories.*;
+import dev.mayra.seeddesafiocdc.services.PurchaseService;
 import dev.mayra.seeddesafiocdc.utils.exceptions.InvalidRequestException;
 import dev.mayra.seeddesafiocdc.utils.exceptions.NotFoundException;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,54 +27,18 @@ import java.util.stream.Collectors;
 @RequestMapping("/purchase")
 @Tag(name = "Purchase", description = "Operations related to the Purchase entity")
 public class PurchaseController {
+    private final PurchaseService purchaseService;
     private final PurchaseRepository purchaseRepository;
-    private final CountryRepository countryRepository;
-    private final StateRepository stateRepository;
-    private final BookRepository bookRepository;
-    private final CouponRepository couponRepository;
 
-    public PurchaseController(PurchaseRepository purchaseRepository, CountryRepository countryRepository, StateRepository stateRepository, BookRepository bookRepository, CouponRepository couponRepository) {
+    public PurchaseController(PurchaseService purchaseService, PurchaseRepository purchaseRepository) {
+        this.purchaseService = purchaseService;
         this.purchaseRepository = purchaseRepository;
-        this.countryRepository = countryRepository;
-        this.stateRepository = stateRepository;
-        this.bookRepository = bookRepository;
-        this.couponRepository = couponRepository;
     }
 
     @PostMapping
     public ResponseEntity<PurchaseResponseDTO> create(@RequestBody @Valid PurchaseRequestDTO purchase) {
-        Country country = countryRepository.findById(purchase.getCountryId())
-            .orElseThrow(() -> new NotFoundException("Country not found"));
-
-        State state = null;
-
-        if(country.hasStates()) {
-            state = stateRepository.findById(purchase.getStateId())
-                .orElseThrow(() -> new NotFoundException("State not found"));
-        }
-
-        Purchase created = new Purchase(purchase, country, state);
-        List<PurchaseItem> itemsEntity = itemRequestListToItem(purchase);
-        created.addAllItems(itemsEntity);
-
-        if(!created.isEqualToCalculatedSubtotal(purchase.getTotal())) {
-            throw new InvalidRequestException("Total given is different than calculated total");
-        }
-
-        if(Optional.ofNullable(purchase.getCouponCode()).isPresent()) {
-            Coupon coupon = couponRepository.findByCode(purchase.getCouponCode())
-                .orElseThrow(() -> new NotFoundException("Coupon not found"));
-
-            ValidationResult validation = created.applyDiscount(coupon);
-
-            if(!validation.isValid()) {
-                throw new InvalidRequestException(validation.getErrorMessage());
-            }
-        }
-
-        Purchase saved = purchaseRepository.save(created);
-
-        return new ResponseEntity<>(saved.toResponseDTO(), HttpStatus.CREATED);
+        return ResponseEntity.ok().body(purchaseService.create(purchase)
+            .toResponseDTO());
     }
 
     @GetMapping
@@ -99,31 +64,4 @@ public class PurchaseController {
         return ResponseEntity.ok().body(new PurchaseMinifiedDTO(found));
     }
 
-    private List<PurchaseItem> itemRequestListToItem(PurchaseRequestDTO dto) {
-        List<Long> bookIds = extractBookIds(dto);
-
-        Map<Long, Book> bookMap = bookRepository.findAllById(bookIds).stream()
-            .collect(Collectors.toMap(Book::getId, book -> book));
-
-        validateBooksExist(bookIds, bookMap);
-
-        return dto.getItems().stream()
-            .map(item -> new PurchaseItem(bookMap.get(item.getBookId()), item.getQuantity()))
-            .toList();
-    }
-
-    private List<Long> extractBookIds(PurchaseRequestDTO dto) {
-        return dto.getItems().stream()
-            .map(PurchaseItemRequestDTO::getBookId)
-            .toList();
-    }
-
-    private void validateBooksExist(List<Long> bookIds, Map<Long, Book> bookMap) {
-        if (bookIds.size() != bookMap.size()) {
-            List<Long> missingIds = bookIds.stream()
-                .filter(id -> !bookMap.containsKey(id))
-                .toList();
-            throw new NotFoundException("Books not found with ids: " + missingIds);
-        }
-    }
 }
---
+++ src/main/java/dev/mayra/seeddesafiocdc/services/PurchaseService.java
@@ -0,0 +1,117 @@
+package dev.mayra.seeddesafiocdc.services;
+
+import dev.mayra.seeddesafiocdc.model.book.Book;
+import dev.mayra.seeddesafiocdc.model.country.Country;
+import dev.mayra.seeddesafiocdc.model.coupon.Coupon;
+import dev.mayra.seeddesafiocdc.model.purchase.Purchase;
+import dev.mayra.seeddesafiocdc.model.purchase.PurchaseRequestDTO;
+import dev.mayra.seeddesafiocdc.model.purchaseItem.PurchaseItem;
+import dev.mayra.seeddesafiocdc.model.purchaseItem.PurchaseItemRequestDTO;
+import dev.mayra.seeddesafiocdc.model.state.State;
+import dev.mayra.seeddesafiocdc.model.validation.ValidationResult;
+import dev.mayra.seeddesafiocdc.repositories.*;
+import dev.mayra.seeddesafiocdc.utils.exceptions.InvalidRequestException;
+import dev.mayra.seeddesafiocdc.utils.exceptions.NotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class PurchaseService {
+    private final PurchaseRepository purchaseRepository;
+    private final CountryRepository countryRepository;
+    private final StateRepository stateRepository;
+    private final BookRepository bookRepository;
+    private final CouponRepository couponRepository;
+
+    public PurchaseService(PurchaseRepository purchaseRepository, CountryRepository countryRepository,
+                           StateRepository stateRepository, BookRepository bookRepository,
+                           CouponRepository couponRepository) {
+        this.purchaseRepository = purchaseRepository;
+        this.countryRepository = countryRepository;
+        this.stateRepository = stateRepository;
+        this.bookRepository = bookRepository;
+        this.couponRepository = couponRepository;
+    }
+
+    public Purchase create(PurchaseRequestDTO dto) {
+        Country country = countryRepository.findById(dto.getCountryId())
+            .orElseThrow(() -> new NotFoundException("Country not found"));
+
+        Optional<State> possibleState = getStateIfCountryHasStates(country, dto);
+
+        Purchase created = new Purchase(dto, country, possibleState.orElse(null));
+
+        addItemsToPurchase(created, dto);
+
+        if(!created.isEqualToCalculatedSubtotal(dto.getTotal())) {
+            throw new InvalidRequestException("Total given is different than calculated total");
+        }
+
+        applyCouponIfCouponCodeExists(dto, created);
+
+        return purchaseRepository.save(created);
+
+    }
+
+    private void addItemsToPurchase(Purchase created, PurchaseRequestDTO dto) {
+        List<PurchaseItem> itemsEntity = itemRequestListToItem(dto);
+        created.addAllItems(itemsEntity);
+    }
+
+    private void applyCouponIfCouponCodeExists(PurchaseRequestDTO dto, Purchase created) {
+        if(Optional.ofNullable(dto.getCouponCode()).isPresent()) {
+            Coupon coupon = couponRepository.findByCode(dto.getCouponCode())
+                .orElseThrow(() -> new NotFoundException("Coupon not found"));
+
+            ValidationResult validation = created.applyDiscount(coupon);
+
+            if(!validation.isValid()) {
+                throw new InvalidRequestException(validation.getErrorMessage());
+            }
+        }
+    }
+
+    private List<PurchaseItem> itemRequestListToItem(PurchaseRequestDTO dto) {
+        List<Long> bookIds = extractBookIds(dto);
+
+        Map<Long, Book> bookMap = bookRepository.findAllById(bookIds).stream()
+            .collect(Collectors.toMap(Book::getId, book -> book));
+
+        validateBooksExist(bookIds, bookMap);
+
+        return dto.getItems().stream()
+            .map(item -> new PurchaseItem(bookMap.get(item.getBookId()), item.getQuantity()))
+            .toList();
+    }
+
+    private List<Long> extractBookIds(PurchaseRequestDTO dto) {
+        return dto.getItems().stream()
+            .map(PurchaseItemRequestDTO::getBookId)
+            .toList();
+    }
+
+    private Optional<State> getStateIfCountryHasStates(Country country, PurchaseRequestDTO dto) {
+        if(country.hasStates()) {
+            State state = stateRepository.findById(dto.getStateId())
+                .orElseThrow(() -> new NotFoundException("State not found"));
+
+            return Optional.of(state);
+        }
+
+        return Optional.empty();
+    }
+
+    private void validateBooksExist(List<Long> bookIds, Map<Long, Book> bookMap) {
+        if (bookIds.size() != bookMap.size()) {
+            List<Long> missingIds = bookIds.stream()
+                .filter(id -> !bookMap.containsKey(id))
+                .toList();
+            throw new NotFoundException("Books not found with ids: " + missingIds);
+        }
+    }
+
+}
