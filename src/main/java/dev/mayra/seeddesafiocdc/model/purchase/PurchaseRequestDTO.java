b968a8e496bf35c836e81f90b1298859e1914852
Mayra Amaral <mayracunha18@gmail.com>
2024-10-28 00:07:15 -0300

    chore: remove comentário

--- src/main/java/dev/mayra/seeddesafiocdc/model/purchase/PurchaseRequestDTO.java
+++ src/main/java/dev/mayra/seeddesafiocdc/model/purchase/PurchaseRequestDTO.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
